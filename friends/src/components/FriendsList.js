43fc3f735fa42d89d67f4ce45d3cc71cb72d1632
zachary-peasley <zpman95@hotmail.com>
2019-09-17 15:46:21 -0500

    Added FriendsList

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,58 +1,42 @@
 import React from 'react';
-import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Friend from './Friends';
+import FriendsForm from './FriendsForm';
 
+class FriendList extends React.Component {
+    state = {
+        friends: []
+    };
 
-class Login extends React.Component {
-  state = {
-    credentials: {
-      username: '',
-      password: ''
-    }
-  };
-
-  handleChange = e => {
-    this.setState({
-      credentials: {
-        ...this.state.credentials,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  login = e => {
-    e.preventDefault();
-    axiosWithAuth()
-    .post
-    ('http://localhost:5000/api/login', this.state.credentials)
-    .then(res =>  { console.log(res.data)
-        localStorage.setItem('token', res.data.payload);
-        this.props.history.push('.friendslist')
-    })
-
-    .catch(err => console.log(err.response));
-  };
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.login}>
-          <input
-            type="text"
-            name="username"
-            value={this.state.credentials.username}
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            value={this.state.credentials.password}
-            onChange={this.handleChange}
-          />
-          <button>Log in</button>
-        </form>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.getData();
 }
 
-export default Login;
+    getData = () => {
+        axiosWithAuth()
+        .get('http://localhost:5000/api/friends')
+        .then(res => {
+            console.log(res.data);
+            this.setState({
+               ...this.state,
+               friends: res.data
+            })
+        })
+        .catch(error => console.log(error.response));
+    }
+ render() {
+        return (
+            <div className='friends-container'>
+                <div className='friends-list'>
+                    <h2>Friends:</h2>
+                    {this.state.friends.map(friends => {
+                        return <Friend key={friend.id} name={friend.name} email={email} />
+                    })}
+                </div>
+                <FriendList />
+            </div>
+        )
+    }
+}
+
+export default FriendList;
