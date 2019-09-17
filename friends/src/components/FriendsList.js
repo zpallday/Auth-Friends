1151d7843be8737bf0db7a410349b3eaa15a51c9
zachary-peasley <zpman95@hotmail.com>
2019-09-17 15:18:57 -0500

    Got to transfer it to login

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post
+    ('http://localhost:5000/api/login', this.state.credentials)
+    .then(res =>  { console.log(res.data)
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('.friendslist')
+    })
+
+    .catch(err => console.log(err.response));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
